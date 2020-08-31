4bb996d6440af1982763ab3236bf665b36f9795b
Aleksandras Šukelovič <alex.shukel@gmail.com>
2020-08-31 10:43:25 +0300

    added breakpoints in TodoGroups

--- src/components/TodoGroupsList.tsx
+++ src/components/TodoGroupsList.tsx
@@ -5,9 +5,8 @@ import {
 } from './TodoContext/TodoArrayHelper';
 import { TodoGroup } from '../objects/TodoGroup';
 import Group from './Group';
-import styles from './TodoGroupsList.scss';
 import FormPopup from './FormPopup';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import NewGroupForm from './NewGroupForm';
 
 const TodoGroupsList = () => {
@@ -20,17 +19,32 @@ const TodoGroupsList = () => {
         <TodoArrayHelper arrayPath={`groups`}>
             {(controller: ArrayController<TodoGroup>) => {
                 return (
-                    <div className={styles.groupsList}>
-                        {controller.array.map((group: TodoGroup) => (
-                            <Group groupId={group.id} key={group.id} />
-                        ))}
-                        <FormPopup open={open} onClickAway={closeForm}>
-                            <div style={{ width: 400 }}>
-                                <NewGroupForm closePopup={closeForm} />
-                            </div>
-                        </FormPopup>
-                        <Button onClick={openForm}>Add new group</Button>
-                    </div>
+                    <Grid container spacing={2}>
+                        <Grid container spacing={2}>
+                            {controller.array.map((group: TodoGroup) => (
+                                <Grid
+                                    item
+                                    xs={12}
+                                    sm={6}
+                                    md={4}
+                                    lg={3}
+                                    xl={2}
+                                    container
+                                    justify="center"
+                                >
+                                    <Group groupId={group.id} key={group.id} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                        <Grid container>
+                            <FormPopup open={open} onClickAway={closeForm}>
+                                <div style={{ width: 400 }}>
+                                    <NewGroupForm closePopup={closeForm} />
+                                </div>
+                            </FormPopup>
+                            <Button onClick={openForm}>Add new group</Button>
+                        </Grid>
+                    </Grid>
                 );
             }}
         </TodoArrayHelper>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 750,
+            md: 1100,
+            lg: 1450,
+            xl: 2150,
+        },
+    },
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+    document.getElementById('root')
+);
